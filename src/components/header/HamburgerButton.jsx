61174edad612b07cac2c69c4e6f1caf5f93fda05
Alejandro <Alejandrocanals17@gmail.com>
2024-08-26 20:07:07 +0200

    Added header

---
+++ src/components/header/HamburgerButton.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import ToogleMenu from "./ToogleMenu";
+const HamburgerButton = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	function toggleMenu() {
+		setIsOpen(!isOpen);
+	}
+	return (
+		<div>
+			<button className="btn btn-square btn-ghost" onClick={toggleMenu}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					className="inline-block h-5 w-5 stroke-current"
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					></path>
+				</svg>
+			</button>
+			{isOpen && <ToogleMenu />}
+		</div>
+	);
+};
+
+export default HamburgerButton;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
-import PackList from "../components/packs/PackList"
+import Header from "../components/header/Header"
 import Hero from "../components/hero/Hero"
+import PackList from "../components/packs/PackList"
 import { packsData } from "../data/packsData"
 
 const HomePage = () => {
 	return (
 		<div>
 			{/* Esto es para ir viend como queda */}
+			<Header />
 			<Hero />
 			<PackList packs={packsData} />
 		</div>
