acd0d4b7320ec03f32aa1759c9e6bc224ab17573
Antonio Abril <antonio.abril@octoenergy.com>
2024-08-27 20:25:11 +0200

    Remove unused code in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react"
 import HomePage from "./pages/HomePage"
 
 import "./App.css"
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<>
 			<HomePage />
