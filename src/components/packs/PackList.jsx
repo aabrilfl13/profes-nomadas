3f82645d07141a7ace07d05fdf50b3cf86d22b9c
Alejandro Canals <alejandrocanals17@gmail.com>
2024-09-28 21:25:07 +0200

    add validation to packlist

--- src/components/packs/PackList.jsx
+++ src/components/packs/PackList.jsx
@@ -1,15 +1,30 @@
+import PropTypes from "prop-types"
 import Pack from "@/components/packs/Pack"
 
 const PackList = ({ packs }) => {
 	return (
 		<div className="pack-list mt-5 flex flex-col items-center">
 			<h1 className="text-2xl">Nuestros Planes</h1>
-			{/* Loop through the packs that are inserted from the packsData */}
 			{packs.map((pack, index) => (
 				<Pack key={index} {...pack} />
 			))}
 		</div>
 	)
+}
+
+PackList.propTypes = {
+	packs: PropTypes.arrayOf(
+		PropTypes.shape({
+			name: PropTypes.string.isRequired,
+			image: PropTypes.string.isRequired,
+			prices: PropTypes.shape({
+				basic: PropTypes.number,
+				medium: PropTypes.number,
+				premium: PropTypes.number,
+				extraPremium: PropTypes.number,
+			}).isRequired,
+		})
+	).isRequired,
 }
 
 export default PackList
